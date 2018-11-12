20ded630d756cbea0a4565d2b54edc9e3c0ba8de
Artur Pietrucha <30773740+LED1990@users.noreply.github.com>
2018-11-12 11:18:11 +0100

    zrobioe zglosznie alfa- początek prac nad docelowym gui

--- springBackend/src/main/java/main/SpringAngularApplication.java
+++ springBackend/src/main/java/main/SpringAngularApplication.java
@@ -1,19 +1,14 @@
 package main;
 
-import main.Model.Choroba;
-import main.Model.alfa.ZgloszenieAlfa;
 import main.repository.daoimpl.ZgloszenieAlfaServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.transaction.Transactional;
-
 @SpringBootApplication
-public class SpringAngularApplication implements CommandLineRunner {
+public class SpringAngularApplication{
 
     private static final Logger logger = LogManager.getLogger(ZgloszenieAlfaServiceImpl.class);
 
@@ -24,18 +19,5 @@ public class SpringAngularApplication implements CommandLineRunner {
         SpringApplication.run(SpringAngularApplication.class, args);
     }
 
-    /**
-     * @Transactional pozwala utworzenie sessji do polaczneia zbaza danych
-     * @param arg0
-     * @throws Exception
-     */
-    @Override
-    @Transactional
-    public void run(String... arg0) throws Exception {
-        ZgloszenieAlfa zgloszenieAlfa = zgloszenieAlfaDao.pobierzPoId(18L);
-        for (Choroba ch:zgloszenieAlfa.getChoroba()
-             ) {
-            logger.info("zgłoszenie: "+zgloszenieAlfa.getId()+" choroba: "+ch.getNazwa()+" data "+ch.getDataWystapienia());
-        }
-    }
+
 }
